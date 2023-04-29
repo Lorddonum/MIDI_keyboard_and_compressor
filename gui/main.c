a71f9aea6701c087393cfaee1852a79b5f262865
ttyyls <127455904+ttyyls@users.noreply.github.com>
2023-04-29 10:21:30 +0100

    changes: started flushing out the layout

--- gui/main.c
+++ gui/main.c
@@ -25,20 +25,77 @@
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 800
 
-struct settings {
-
+// struct settings {};
+struct nk_canvas {
+  struct nk_command_buffer *painter;
+  struct nk_vec2 item_spacing;
+  struct nk_vec2 panel_padding;
+  struct nk_style_item window_background;
 };
 
-static void keyboard(struct nk_context *ctx) {
-  if (nk_begin(ctx, "keyboard", nk_rect(10, 10, 180, 250),
-               NK_WINDOW_BORDER | NK_WINDOW_NO_SCROLLBAR | NK_WINDOW_MOVABLE)) {
+static nk_bool canvas_begin(struct nk_context *ctx, struct nk_canvas *canvas,
+                            const char *title, nk_flags flags, int x, int y,
+                            int width, int height,
+                            struct nk_color background_color) {
+  /* save style properties which will be overwritten */
+  canvas->panel_padding = ctx->style.window.padding;
+  canvas->item_spacing = ctx->style.window.spacing;
+  canvas->window_background = ctx->style.window.fixed_background;
+
+  /* use the complete window space and set background */
+  ctx->style.window.spacing = nk_vec2(0, 0);
+  ctx->style.window.padding = nk_vec2(0, 0);
+  ctx->style.window.fixed_background = nk_style_item_color(background_color);
+
+  /* create/update window and set position + size */
+  if (!nk_begin(ctx, title, nk_rect(x, y, width, height),
+                NK_WINDOW_NO_SCROLLBAR | flags))
+    return nk_false;
+
+  /* allocate the complete window space for drawing */
+  {
+    struct nk_rect total_space;
+    total_space = nk_window_get_content_region(ctx);
+    nk_layout_row_dynamic(ctx, total_space.h, 1);
+    nk_widget(&total_space, ctx);
+    canvas->painter = nk_window_get_canvas(ctx);
   }
+
+  return nk_true;
+}
+
+static void canvas_end(struct nk_context *ctx, struct nk_canvas *canvas) {
   nk_end(ctx);
+  ctx->style.window.spacing = canvas->panel_padding;
+  ctx->style.window.padding = canvas->item_spacing;
+  ctx->style.window.fixed_background = canvas->window_background;
+}
+
+static void keyboard(struct nk_context *ctx) {
+  struct nk_canvas canvas;
+  if (canvas_begin(ctx, &canvas, "keyboard",
+                   NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_TITLE, 10,
+                   10, 1200, 700, nk_rgb(250, 250, 250))) {
+    float x = canvas.painter->clip.x, y = canvas.painter->clip.y;
+
+    nk_fill_rect(canvas.painter, nk_rect(x + 700 - 210, y + 210, 210, 210), 5,
+                 nk_rgb(247, 230, 154));
+    nk_fill_rect(canvas.painter, nk_rect(x, y, 210, 210), 5,
+                 nk_rgb(247, 230, 154));
+    nk_fill_rect(canvas.painter, nk_rect(x, y, 210, 210), 5,
+                 nk_rgb(247, 230, 154));
+    nk_fill_rect(canvas.painter, nk_rect(x, y, 210, 210), 5,
+                 nk_rgb(247, 230, 154));
+    nk_fill_rect(canvas.painter, nk_rect(x, y, 210, 210), 5,
+                 nk_rgb(247, 230, 154));
+  }
+  canvas_end(ctx, &canvas);
 }
 
 static void player(struct nk_context *ctx) {
-  if (nk_begin(ctx, "player", nk_rect(10, 10, 180, 250),
-               NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_NO_SCROLLBAR)) {
+  if (nk_begin(ctx, "Player", nk_rect(10, 10, 180, 250),
+               NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_NO_SCROLLBAR |
+                   NK_WINDOW_TITLE)) {
   }
   nk_end(ctx);
 }
