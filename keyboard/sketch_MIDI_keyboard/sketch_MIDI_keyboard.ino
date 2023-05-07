ba6683520aee6f3c84fde649493cc25500ac3ead
donum <donum@localhost.localdomain>
2023-05-07 17:29:32 +0100

    keyboard update2

--- keyboard/sketch_MIDI_keyboard/sketch_MIDI_keyboard.ino
+++ keyboard/sketch_MIDI_keyboard/sketch_MIDI_keyboard.ino
@@ -60,98 +60,98 @@ void loop(void) {
   }
 
   if(digitalRead(Pin_D4)){ //d
-    buffer[2]=20;
+    buffer[2]=7;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_D5)){ //f
-    buffer[2]=20;
+    buffer[2]=9;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_D7)){ //g
-    buffer[2]=20;
+    buffer[2]=10;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_D9)){ //h
-    buffer[2]=20;
+    buffer[2]=11;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_D11)){ //j
-    buffer[2]=20;
+    buffer[2]=13;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_D12)){ //k
-    buffer[2]=20;
+    buffer[2]=14;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_A2)){ //l
-    buffer[2]=20;
+    buffer[2]=15;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_A4)){ //;
-    buffer[2]=20;
+    buffer[2]=51;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_A5)){ //'
-    buffer[2]=20;
+    buffer[2]=52;
     Serial.write(buffer,8);
     release();
   }
 
   //BLACK KEYS
   if(digitalRead(Pin_D1)){ //w
-    buffer[2]=20;
+    buffer[2]=26;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_D3)){ //e
-    buffer[2]=20;
+    buffer[2]=8;
     Serial.write(buffer,8);
     release();
   }
 
   if(digitalRead(Pin_D6)){ //t
-    buffer[2]=20;
+    buffer[2]=23;
     Serial.write(buffer,8);
     release();
   }
 
-  if(digitalRead(Pin_D8)){ //u
-    buffer[2]=20;
+  if(digitalRead(Pin_D8)){ //y
+    buffer[2]=28;
     Serial.write(buffer,8);
     release();
   }
 
-  if(digitalRead(Pin_D10)){ //i
-    buffer[2]=20;
+  if(digitalRead(Pin_D10)){ //u
+    buffer[2]=24;
     Serial.write(buffer,8);
     release();
   }
 
-  if(digitalRead(Pin_A1)){ //p
-    buffer[2]=20;
+  if(digitalRead(Pin_A1)){ //o
+    buffer[2]=18;
     Serial.write(buffer,8);
     release();
   }
 
-  if(digitalRead(Pin_A3)){ //]
-    buffer[2]=20;
+  if(digitalRead(Pin_A3)){ //p
+    buffer[2]=19;
     Serial.write(buffer,8);
     release();
   }
